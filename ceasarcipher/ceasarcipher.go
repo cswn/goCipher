c386f81b5d7a968df4dae97323c46301e749a0b0
Clara Swanson <claraswanson671@gmail.com>
2023-03-28 21:06:26 +0200

    Implemented a kinda icky way of keeping within rune range of lowercase letters

--- ceasarcipher/ceasarcipher.go
+++ ceasarcipher/ceasarcipher.go
@@ -10,14 +10,23 @@ func ShiftText(plainText string, shift int) string {
 	cipherText := ""
 	plainText = strings.ToLower(plainText)
 	// Still doesn't work when you choose a key that is under 26, but
-	// still goes past rune num 122 (which is z)
+	// still goes past rune 122 (which is z)
+	// need to define range of runes that it should stay within (97 -122)
 
 	for _, letter := range plainText {
 		if !unicode.IsLetter(letter) {
 			cipherText += string(letter)
 		} else {
-			newutf8 := (int(letter) + (shift % 26))
-			cipherText += string(rune(newutf8))
+			var newPos int
+			if (int(letter) + shift) > 122 {
+				newPos = (97 + (shift % 26))
+			} else if (int(letter) + shift) < 97 {
+				newPos = (122 + (shift % 26))
+			} else {
+				newPos = (int(letter) + (shift % 26))
+			}
+
+			cipherText += string(rune(newPos))
 		}
 	}
 
